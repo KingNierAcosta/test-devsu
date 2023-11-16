813abed85df95d05411b222f1bbadc721a2007e6
leadfront <leadfront@vixidev.com>
2023-11-16 01:27:33 -0500

    Add pagination

--- src/app/pages/products/data-source.ts
+++ src/app/pages/products/data-source.ts
@@ -5,8 +5,12 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class DataSourceProduct extends DataSource<Product> {
 
   data = new BehaviorSubject<Product[]>([]);
-  originalData: Product[] = [];
-  size: number = 5;
+  private _originalData: Product[] = [];
+  private _filterData: Product[] = [];
+  private _size: number = 5;
+  private _totalPage!: number;
+  private _currentPage: number = 1;
+  private _query: string = '';
 
   override connect(collectionViewer: CollectionViewer): Observable<readonly Product[]> {
     return this.data;
@@ -17,34 +21,53 @@ export class DataSourceProduct extends DataSource<Product> {
   }
 
   init(products: Product[], size: number) {
-    this.size = size;
+    products= [].concat(...Array(10).fill(products));
+    this._size = size;
+    this._totalPage = Math.ceil(products.length / size);
+    this._currentPage = 1;
+    this._originalData = products;
+    this._filterData = products;
     this.data.next(products.slice(0, size));
-    this.originalData = products;
   }
 
   update(id: string, changes: Partial<Product>) {
-    const products = this.data.getValue();
-    const index = products.findIndex(it => it.id === id);
-    if (index !== -1) {
-      products[index] = {
-        ...products[index],
-        ...changes
-      }
-      this.data.next(products);
+    const index = this._originalData.findIndex(it => it.id === id);
+    if (index === -1) return;
+
+    this._originalData[index] = {
+      ...this._originalData[index],
+      ...changes
+    }
+    this.verificateQueryAndUpdateData();
+  }
+
+  private verificateQueryAndUpdateData() {
+    if (this._query.length) {
+      const query = this._query;
+      this._query = '';
+      this.find(query);
+    } else {
+      this._filterData = this._originalData;
+      this.updatePagination();
     }
   }
 
+
   find(query: string) {
-    const newProducts = this.originalData.filter(it => {
+    const lowerCaseQuery = query.trim().toLowerCase();
+    if (lowerCaseQuery === this._query) return;
+    this._query = lowerCaseQuery;
+
+    this._filterData = this._originalData.filter(it => {
       const word = `${it.name}-${it.description}-${it.date_release}-${it.date_revision}`;
-      return word.toLowerCase().includes(query.toLowerCase())
+      return word.toLowerCase().includes(this._query)
     });
-    this.data.next(newProducts);
+    this.updatePagination();
   }
 
   delete(id: string) {
-    this.originalData = this.originalData.filter(it => it.id.toLowerCase() !== id.toLowerCase());
-    this.data.next(this.originalData.slice(0, this.size));
+    this._originalData = this._originalData.filter(it => it.id.toLowerCase() !== id.toLowerCase());
+    this.verificateQueryAndUpdateData();
   }
 
   get count() {
@@ -52,11 +75,57 @@ export class DataSourceProduct extends DataSource<Product> {
   }
 
   get total() {
-    return this.originalData.length;
+    return this._filterData.length;
+  }
+
+  get totalPage() {
+    return this._totalPage || 0;
+  }
+
+  get currentPage() {
+    return this._currentPage;
   }
 
   changePageSize(size: number) {
-    this.data.next(this.originalData.slice(0, size));
+    this._size = size;
+    const query = this._query;
+    this._query = '';
+    !query.length ? this.resetData() : this.find(query);
+  }
+
+  get hasPrev(): boolean {
+    return this._currentPage > 1;
+  }
+
+  prev() {
+    if (!this.hasPrev) return;
+
+    this._currentPage--;
+    this.updatePagination();
+  }
+
+  get hasNext(): boolean {
+    return this._currentPage < this._totalPage;
+  }
+
+  next() {
+    if (!this.hasNext) return;
+
+    this._currentPage++;
+    this.updatePagination();
+  }
+
+  private updatePagination() {
+    this._totalPage = Math.ceil(this._filterData.length / this._size);
+    const startIndex = (this._currentPage - 1) * this._size;
+    const endIndex = startIndex + this._size;
+    this.data.next(this._filterData.slice(startIndex, endIndex));
+  }
+
+  private resetData() {
+    this._filterData = this._originalData;
+    this._currentPage = 1;
+    this.updatePagination();
   }
 
 }
